d00ed02451312960b4e416904948b3f9b816999a
woodrowdeveloper <woodrowdeveloper@gmail.com>
2021-09-24 21:43:39 -0400

    Add project files.

---
+++ Features/Login.feature
@@ -0,0 +1,11 @@
+﻿Feature: Login
+	Login to EA Demo application
+
+@smoke
+Scenario: Perform Login of EA Application site
+	Given I launch the application
+	And I enter the following details
+	| UserName | Password |
+	| admin    | password   |
+	And I click login button
+	Then I should see Employee details link
---
+++ Steps/LogInSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecflowNetCoreDemo.Steps
+{
+    [Binding]
+    public sealed class LogInSteps
+    {
+        [Given(@"I launch the application")]
+        public void GivenILaunchTheApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I enter the following details")]
+        public void GivenIEnterTheFollowingDetails(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I click login button")]
+        public void GivenIClickLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see Employee details link")]
+        public void ThenIShouldSeeEmployeeDetailsLink()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
